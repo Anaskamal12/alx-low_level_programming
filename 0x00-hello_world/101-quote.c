5de66db4efa28263e98cf31f0b96469576a5a1e9
Anaskamal12 <anaskamal748@gmail.com>
2023-09-07 14:20:54 -0700

    Hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
-* main - Entry point
-*
+* main - A c program that a line to the standard error
 * Return: Always 1 (Success)
 */
 int main(void)
 {
-        write(2,
-	"and that piece of art is useful\"-Dora Kopar, 2015-10-19\n",59);
-        return (1);
-}	
+	fput("and that piece of art is useful\" -Dora Kopar, 2015-10-19\n", stdout);
+	return (1);
+}
