c60a43e42d65febfcf05f685fb4fcde0cbdd4beb
Anaskamal12 <anaskamal748@gmail.com>
2023-09-08 09:11:28 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
+
 /**
- * main - prints the alphabetic
+ * main - Prints the alphabetic
  * Return: Always (Success)
  */
+
 int main(void)
+
 {
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
 	putchar(0);
 	putchar('\n');
-return (0);
+	return (0);
+
 }
