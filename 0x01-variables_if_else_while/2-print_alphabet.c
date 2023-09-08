81a8b17d97913350d8c562b0a3944a5b847f61a4
Anaskamal12 <anaskamal748@gmail.com>
2023-09-08 08:49:10 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabetic
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putcher(0);
+	putcher('\n');
+return (0);
+}
